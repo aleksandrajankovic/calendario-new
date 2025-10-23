36eedd82c4c7c74248157750fc235d283f28b31c
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2025-10-23 11:04:50 +0200

    Update specialPormotions.js

--- assets/js/specialPormotions.js
+++ assets/js/specialPormotions.js
@@ -89,6 +89,93 @@ const specialPromotions = {
       link: "https://meridianbet.bet.br/ca/inicio",
       button: "Saiba mais",
     },
+      26: {
+    title: "PALMEIRAS X CRUZEIRO",
+    subtitle: "",
+    image: "./assets/images/header/26oct.png",
+    headerClass: "left",
+    description: [
+      "Aposte pelo menos R$ 10,00 no evento Palmeiras x Cruzeiro válido pelo Brasileirão Série A no dia 26/10/2025 e receba 100% do valor em freebet e + 50 rodadas grátis no jogo Cachorro Sortudo!",
+      "Qualquer aposta no jogo citado será válida desde que esteja de acordo com as condições e termos de uso citados na página da promoção.",
+      "As recompensas serão disponibilizadas no dia 27/10/2025 às 11h.",
+    ],
+
+    link: "https://meridianbet.bet.br/ca/inicio",
+    button: "Saiba mais",
+  },
+  27: {
+    title: "WILD WHITE WHALE",
+    subtitle: "",
+    image: "./assets/images/header/27oct.png",
+    headerClass: "left",
+    description: [
+      "Aposte R$ 25,00 no jogo WILD WHITE WHALE e ganhe 50 rodadas grátis no mesmo jogo!!",
+      "Válido somente para os clientes que jogarem no dia 27/10/2025.",
+      "As rodadas serão disponibilizadas imediatamente no mesmo jogo.",
+      "Você tem 24 horas para utilizar as rodadas grátis após aceitar a notificação.",
+    ],
+
+    link: "https://meridianbet.bet.br/ca/inicio",
+    button: "Saiba mais",
+  },
+  28: {
+    title: "ZOMBIE APOCALYPSE",
+    subtitle: "",
+    image: "./assets/images/header/28oct.png",
+    headerClass: "left",
+    description: [
+      "Aposte R$ 50,00 no jogo ZOMBIE APOCALYPSE e ganhe 100 rodadas grátis no mesmo jogo!!",
+      "Válido somente para os clientes que jogarem no dia 28/10/2025.",
+      "As rodadas serão disponibilizadas imediatamente no mesmo jogo",
+      "Você tem 24 horas para utilizar as rodadas grátis após aceitar a notificação.",
+    ],
+
+    link: "https://meridianbet.bet.br/ca/inicio",
+    button: "Saiba mais",
+  },
+  29: {
+    title: " RACING X FLAMENGO",
+    subtitle: "",
+    image: "./assets/images/header/29oct.png",
+    headerClass: "left",
+    description: [
+      "Aposte pelo menos R$ 10,00 no evento Racing x Flamengo válido pela Libertadores no dia 29/10/2025 e receba 100% do valor em freebet e + 50 rodadas grátis no jogo Mining Rush!",
+      "Qualquer aposta no jogo citado será válida desde que esteja de acordo com as condições e termos de uso citados na página da promoção.",
+      "As recompensas serão disponibilizadas no dia 30/10/2025 às 11h.",
+    ],
+
+    link: "https://meridianbet.bet.br/ca/inicio",
+    button: "Saiba mais",
+  },
+  30: {
+    title: "PALMEIRAS  X LDU",
+    subtitle: "",
+    image: "./assets/images/header/30oct.png",
+    headerClass: "left",
+    description: [
+      "Aposte pelo menos R$ 10,00 no evento Palmeiras x LDU válido pela Libertadores no dia 30/10/2025 e receba 100% do valor em freebet e + 50 rodadas grátis no jogo Rainbow Gold!",
+      "Qualquer aposta no jogo citado será válida desde que esteja de acordo com as condições e termos de uso citados na página da promoção.",
+      "As recompensas serão disponibilizadas no dia 31/10/2025 às 11h.",
+    ],
+
+    link: "https://meridianbet.bet.br/ca/inicio",
+    button: "Saiba mais",
+  },
+   31: {
+    title: "VENI VIDI VICI",
+    subtitle: "",
+    image: "./assets/images/header/31oct.png",
+    headerClass: "left",
+    description: [
+      "Aposte R$ 75,00 no jogo VENI VIDI VICI e ganhe 150 rodadas grátis no mesmo jogo!!",
+      "Válido somente para os clientes que jogarem no dia 31/10/2025.",
+      "As rodadas serão disponibilizadas imediatamente no mesmo jogo.",
+      "Você tem 24 horas para utilizar as rodadas grátis após aceitar a notificação.",
+    ],
+
+    link: "https://meridianbet.bet.br/ca/inicio",
+    button: "Saiba mais",
+  },
   };
   
   export default specialPromotions;
